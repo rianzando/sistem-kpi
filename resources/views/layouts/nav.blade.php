1208f0bba19aa3f74ec48dba822a471dab548e1a
rianzando <feriansyah.tp@gmail.com>
2023-07-18 14:31:05 +0700

    add template

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,42 @@
+<div class="container-fluid">
+    <ul class="navbar-nav topbar-nav ml-md-auto align-items-center">
+        <li class="nav-item toggle-nav-search hidden-caret">
+            <a class="nav-link" data-toggle="collapse" href="#search-nav" role="button" aria-expanded="false"
+                aria-controls="search-nav">
+                <i class="fa fa-search"></i>
+            </a>
+        </li>
+        <li class="nav-item dropdown hidden-caret">
+            <a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#" aria-expanded="false">
+                <div class="avatar-sm">
+                    <img src="../assets/img/profile.jpg" alt="..." class="avatar-img rounded-circle">
+                </div>
+            </a>
+            <ul class="dropdown-menu dropdown-user animated fadeIn">
+                <div class="dropdown-user-scroll scrollbar-outer">
+                    <li>
+                        <div class="user-box">
+                            <div class="avatar-lg"><img src="../assets/img/profile.jpg" alt="image profile"
+                                    class="avatar-img rounded"></div>
+                            <div class="u-text">
+                                <h4>Hizrian</h4>
+                                <p class="text-muted">hello@example.com</p><a href="profile.html"
+                                    class="btn btn-xs btn-secondary btn-sm">View Profile</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">My Profile</a>
+                        <a class="dropdown-item" href="#">My Balance</a>
+                        <a class="dropdown-item" href="#">Inbox</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Account Setting</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Logout</a>
+                    </li>
+                </div>
+            </ul>
+        </li>
+    </ul>
+</div>
