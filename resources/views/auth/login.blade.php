8387718658fc9b96f44d00f078fa26688b0c4c34
rianzando <feriansyah.tp@gmail.com>
2023-07-20 22:47:14 +0700

    change login form template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,182 +2,107 @@
 <html lang="en">
 
 <head>
+    <title>Login Sistem KPI</title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Form</title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css">
-    <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <style>
-        body,
-        html {
-            height: 100%;
-            margin: 0;
-            padding: 0;
-            overflow: hidden;
-        }
-
-        #video-background {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-            z-index: -1;
-        }
-
-        .content {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            color: #fff;
-            font-size: 24px;
-        }
-
-        .login-container {
-            width: 400px;
-            background-color: rgba(225, 255, 255, 0.1);
-            /* Form background color */
-            padding: 5;
-            border-radius: 12px;
-            box-shadow: 0 0 10px rgba(225, 255, 255, 0.4);
-        }
-
-        .login-container .card-header {
-            border-bottom: none;
-            text-align: center;
-        }
-
-        /* Animation for form elements */
-        .form-element {
-            opacity: 0;
-            transform: translateY(20px);
-            transition: opacity 0.5s ease, transform 0.5s ease;
-        }
-
-        .form-element.show {
-            opacity: 1;
-            transform: translateY(0);
-        }
-
-        /* Animation for the login button */
-        .login-button {
-            opacity: 0;
-            transform: scale(0.8);
-            transition: opacity 0.5s ease, transform 0.5s ease;
-        }
-
-        .login-button.show {
-            opacity: 1;
-            transform: scale(1);
-        }
-    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    {{-- <link rel="icon" type="image/png" href="images/icons/favicon.ico" /> --}}
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('loginform/vendor/bootstrap/css/bootstrap.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css"
+        href="{{ asset('loginform/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('loginform/vendor/animate/animate.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('loginform/vendor/css-hamburgers/hamburgers.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('loginform/vendor/select2/select2.min.css') }}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('loginform/css/util.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('loginform/css/main.css') }}">
+    <!--===============================================================================================-->
 </head>
 
 <body>
-    <video id="video-background" autoplay loop muted>
-        <source src="{{ asset('backend/images/background.mp4') }}" type="video/mp4">
-    </video>
 
-    <div class="content">
-        <div class="login-container">
-            <div class="card">
-                @if (session('status'))
-                    <div class="alert alert-danger">
-                        {{ session('message') }}
-                    </div>
-                @endif
-                <div class="card-body">
-                    <img src="{{ asset('backend/images/logo-color.png') }}" alt=""
-                        class="rounded mx-auto d-block mb-3" width="250" height="150">
-                    <form action="{{ route('login') }}" method="post">
-                        @csrf
-                        <div class="mb-3">
-                            <label for="email">Email</label>
-                            <input type="email" class="form-control" id="email" name="email" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="password">Password</label>
-                            <div class="input-group">
-                                <input type="password" class="form-control" id="password" name="password" required>
-                                <button type="button" class="btn btn-outline-secondary"
-                                    onclick="togglePassword('password')">
-                                    <i id="eyeIcon" class="fas fa-eye"></i>
-                                </button>
-                            </div>
-                        </div>
-
-                        <div class="mb-3 form-check">
-                            <input type="checkbox" class="form-check-input" id="remember" name="remember">
-                            <label class="form-check-label" for="remember">Remember Me</label>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Login</button>
-                    </form>
+    <div class="limiter">
+        <div class="container-login100">
+            <div class="wrap-login100">
+                <div class="login100-pic js-tilt" data-tilt>
+                    <img src="{{ asset('backend/images/logo-color.png') }}" alt="IMG">
                 </div>
+
+                <form class="login100-form validate-form" action="{{ route('login') }}" method="POST">
+                    @csrf
+                    <span class="login100-form-title">
+                        Login Sistem KPI
+                    </span>
+
+                    <div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+                        <input class="input100" type="text" name="email" placeholder="Email">
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="fa fa-envelope" aria-hidden="true"></i>
+                        </span>
+                    </div>
+
+                    <div class="wrap-input100 validate-input" data-validate="Password is required">
+                        <input class="input100" type="password" name="password" placeholder="Password">
+                        <span class="focus-input100"></span>
+                        <span class="symbol-input100">
+                            <i class="fa fa-lock" aria-hidden="true"></i>
+                        </span>
+                    </div>
+                    <div class="wrap-input100 validate-input">
+                        <input type="checkbox" class="form-check-input" id="remember" name="remember">
+                        <label class="form-check-label" for="remember">Remember Me</label>
+                    </div>
+                    <div class="container-login100-form-btn">
+                        <button class="login100-form-btn" type="submit">
+                            Login
+                        </button>
+                    </div>
+                    {{--
+                    <div class="text-center p-t-12">
+                        <span class="txt1">
+                            Forgot
+                        </span>
+                        <a class="txt2" href="#">
+                            Username / Password?
+                        </a>
+                    </div> --}}
+
+                    <div class="text-center p-t-136">
+                        <a class="txt2" href="#">
+                            IT-Database
+                        </a>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-    <!-- JavaScript -->
-    <script>
-        // Function to toggle password visibility
-        function togglePassword(inputId) {
-            const input = document.getElementById(inputId);
-            const eyeIcon = document.getElementById("eyeIcon");
-            if (input.type === "password") {
-                input.type = "text";
-                eyeIcon.classList.remove("fa-eye");
-                eyeIcon.classList.add("fa-eye-slash");
-            } else {
-                input.type = "password";
-                eyeIcon.classList.remove("fa-eye-slash");
-                eyeIcon.classList.add("fa-eye");
-            }
-        }
-    </script>
-    <script>
-        // Resize the video to maintain aspect ratio
-        window.addEventListener('resize', function() {
-            const video = document.getElementById('video-background');
-            const videoWidth = video.videoWidth;
-            const videoHeight = video.videoHeight;
-            const windowWidth = window.innerWidth;
-            const windowHeight = window.innerHeight;
-            const windowAspectRatio = windowWidth / windowHeight;
-            const videoAspectRatio = videoWidth / videoHeight;
-            if (windowAspectRatio > videoAspectRatio) {
-                video.style.width = '100%';
-                video.style.height = 'auto';
-            } else {
-                video.style.width = 'auto';
-                video.style.height = '100%';
-            }
-        });
-    </script>
 
-    <!-- JavaScript to trigger the animation when the page loads -->
+
+
+
+    <!--===============================================================================================-->
+    <script src="{{ asset('loginform/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
+    <!--===============================================================================================-->
+    <script src="{{ asset('loginform/vendor/bootstrap/js/popper.js') }}"></script>
+    <script src="{{ asset('loginform/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+    <!--===============================================================================================-->
+    <script src="{{ asset('loginform/vendor/select2/select2.min.js') }}"></script>
+    <!--===============================================================================================-->
+    <script src="{{ asset('loginform/vendor/tilt/tilt.jquery.min.js') }}"></script>
     <script>
-        // Function to trigger the form element animation
-        function showFormElements() {
-            const formElements = document.querySelectorAll('.form-element');
-            formElements.forEach((element, index) => {
-                setTimeout(() => {
-                    element.classList.add('show');
-                }, index * 200); // Adjust the delay here to control the animation speed
-            });
-
-            // Trigger the login button animation
-            setTimeout(() => {
-                const loginButton = document.querySelector('.login-button');
-                loginButton.classList.add('show');
-            }, formElements.length * 200 + 100);
-        }
-
-        // Call the function to trigger the animation on page load
-        window.addEventListener('load', showFormElements);
+        $('.js-tilt').tilt({
+            scale: 1.1
+        })
     </script>
+    <!--===============================================================================================-->
+    <script src="{{ asset('loginform/js/main.js') }}"></script>
+
 </body>
 
 </html>
